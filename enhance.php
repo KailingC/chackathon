3dbc69f555113b60181d91370e2082e92ef48565
sexychinese <40586106+sexychinese@users.noreply.github.com>
2019-02-02 16:03:37 -0500

    Update

---
+++ enhance.php
@@ -0,0 +1,23 @@
+
+<?php
+include ('header.inc.php')
+?>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+  <h1 class="h2">Submit Your Questions</h1>
+</div>
+
+<form>
+<input class="form-control form-control-lg" type="text" placeholder="Question">
+<input class="form-control" type="text" placeholder="Answer1">
+<input class="form-control" type="text" placeholder="Answer2">
+<input class="form-control" type="text" placeholder="Answer3">
+<input class="form-control" type="text" placeholder="Answer4">
+<input class="form-control" type="text" placeholder="Answer5">
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+<?php
+include ('footer.inc.php');
+?>
--- index.php
+++ index.php
@@ -12,6 +12,22 @@ Given:<br>
 Find: x<br>
 
 
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+  <h1 class="h2">Submit Your Questions</h1>
+</div>
+Question: <br>
+Answers:<br>
+Box1<br>
+Box1<br>
+Box1<br>
+Box1<br>
+Box1<br>
+<br>
+Submit<br>
+
+
+
+
 
 <?php
 include ('footer.inc.php');
