95c87e349b75de3eca89e9b75bfa7f9f3261c413
sexychinese <40586106+sexychinese@users.noreply.github.com>
2019-02-02 15:13:13 -0500

    Original

---
+++ header.inc.php
@@ -0,0 +1,47 @@
+<html><head>
+<title>Dragonball Math</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
+<link rel="stylesheet" href="https://vote.financegeek.org/style.css">
+</head>
+
+<body>
+
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Dragonball Math</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Home</a>
+    <a class="p-2 text-dark" href="#">Train</a>
+    <a class="p-2 text-dark" href="#">Battle</a>
+    </nav>
+  <a class="btn btn-outline-primary" href="#">Contact</a>
+</div>
+
+
+
+
+
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="">
+              <span data-feather="file"></span>Home
+            </a>
+          </li>
+
+          <blockquote class="blockquote">
+            <img src=ninja.svg width=100%>
+            Character<br>
+          </blockquote>
+            HP: 15/15<br>
+            Exp: 500/5000
+
+        </ul>
+      </div>
+    </nav>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include ('header.inc.php')
+?>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+  <h1 class="h2">Train your Character</h1>
+</div>
+
+
+Given:<br>
+4x+4=24<br>
+<br>
+Find: x<br>
+
+
+
+<?php
+include ('footer.inc.php');
+?>
